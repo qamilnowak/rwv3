a6409d2e115ec3c708d6f1ae218585466835e345
qamilnowak <qamilnowak@gmail.com>
2021-07-24 16:52:05 +0200

    pagination expanded

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link :to="{name: 'EventList'}">Events</router-link> |
-    <router-link :to="{name: 'About'}">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link>
   </div>
   <h1>Events For Good</h1>
   <router-view />
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,19 +3,29 @@
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <router-link
-          id="page-prev"
+        id="page-prev"
         :to="{ name: 'EventList', query: { page: page - 1 } }"
         rel="prev"
         v-if="page !== 1"
         >&#60; Previous</router-link
       >
+      <template v-if="page">
+        <router-link
+          :class="{ 'active-page': page == eventPage }"
+          v-for="eventPage in eventPages"
+          :key="eventPage"
+          :to="{ name: 'EventList', query: { page: eventPage } }"
+        >
+          {{ eventPage }}
+        </router-link>
+      </template>
       <router-link
-          id="page-next"
+        id="page-next"
         :to="{ name: 'EventList', query: { page: page + 1 } }"
         rel="next"
         v-if="hasNextPage"
-        >Next &#62; </router-link
-      >
+        >Next &#62;
+      </router-link>
     </div>
   </div>
 </template>
@@ -24,6 +34,7 @@
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService'
 import { watchEffect } from 'vue'
+import _ from 'lodash'
 
 export default {
   name: 'EventList',
@@ -56,6 +67,9 @@ export default {
     })
   },
   computed: {
+    eventPages() {
+      return _.range(1, Math.ceil(this.totalEvents / 2) + 1)
+    },
     hasNextPage() {
       let totalPages = Math.ceil(this.totalEvents / 2)
       return this.page < totalPages
@@ -86,5 +100,10 @@ export default {
 
 #page-next {
   text-align: right;
+}
+.router-link-active.active-page {
+  font-weight: bold;
+  color: #42b983;
+  text-decoration: underline;
 }
 </style>
